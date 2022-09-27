b36d9f0b2fb39b8b37c6f7c44ba312eb29669ae7
Kate4ka007 <shlemena@mail.ru>
2022-09-28 00:05:56 +0300

    fix: fix mistakes

--- youtube/src/app/app.module.ts
+++ youtube/src/app/app.module.ts
@@ -5,12 +5,12 @@ import AppComponent from './app.component';
 import HeaderComponent from './header/header/header.component';
 import SearchResultsComponent from './search/search-results/search-results.component';
 import SearchItemComponent from './search/search-item/search-item.component';
-import { SearchInputComponent } from './search/search-input/search-input.component';
-import { AuthComponent } from './auth/auth.component';
-import { SearchSortingComponent } from './search/search-sorting/search-sorting.component';
-import { SettingsButtonComponent } from './header/settings-button/settings-button.component';
-import { LogoComponent } from './header/logo/logo.component';
-import { NotFoundComponent } from './shared/not-found/not-found.component';
+import SearchInputComponent from './search/search-input/search-input.component';
+import AuthComponent from './auth/auth.component';
+import SearchSortingComponent from './search/search-sorting/search-sorting.component';
+import SettingsButtonComponent from './header/settings-button/settings-button.component';
+import LogoComponent from './header/logo/logo.component';
+import NotFoundComponent from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
