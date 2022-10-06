5ef0359bacfff5be5c0609c4086209a20cf15023
kate4ka007 <shlemena@mail.ru>
2022-10-06 17:58:34 +0300

    fix: fix sorting

--- youtube/src/app/pipes/sort.pipe.ts
+++ youtube/src/app/pipes/sort.pipe.ts
@@ -7,18 +7,16 @@ import { Item } from '../search/models/search-item.model';
 export default class SortPipe implements PipeTransform {
   // eslint-disable-next-line class-methods-use-this, consistent-return
   transform(value: Item[] | undefined, args?: string): any {
-    // eslint-disable-next-line default-case
     if (value) {
-      // eslint-disable-next-line default-case
-      switch (args) {
-        case 'date': return value.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
-          break;
-        case 'count': return value.sort((a: Item, b: Item) => +a.statistics.viewCount - +b.statistics.viewCount);
-          break;
-        case 'word': return value.sort((a: Item, b: Item) => +a.statistics.commentCount - +b.statistics.commentCount);
-          break;
-        default: return value;
+      if (args) {
+        switch (args) {
+          case 'data': return value.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
+          case 'count': return value.sort((a: Item, b: Item) => +a.statistics.viewCount - +b.statistics.viewCount);
+          // case '': return value;
+          default: return value.filter((a: Item) => a.snippet.tags.join(' ').includes(args!));
+        }
       }
+      return value;
     }
   }
 }
--- youtube/src/app/search/search-sorting/search-sorting.component.ts
+++ youtube/src/app/search/search-sorting/search-sorting.component.ts
@@ -8,10 +8,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export default class SearchSortingComponent {
   isSort!: string;
 
+  word: string = '';
+
   @Output() typeSort = new EventEmitter<string>();
 
   sortBy(str: string) {
     this.isSort = str;
     this.typeSort.emit(this.isSort);
+  }
+
+  sortByWord() {
+    if (this.word) {
+      this.typeSort.emit(this.word);
+    }
   }
 }
