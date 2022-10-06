d05c61d2c6cb170bfa6710ec938c5a9b58f395be
kate4ka007 <shlemena@mail.ru>
2022-10-06 18:09:05 +0300

    fix: fix sorting

--- youtube/src/app/pipes/sort.pipe.ts
+++ youtube/src/app/pipes/sort.pipe.ts
@@ -10,9 +10,8 @@ export default class SortPipe implements PipeTransform {
     if (value) {
       if (args) {
         switch (args) {
-          case 'data': return value.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
+          case 'date': return value.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
           case 'count': return value.sort((a: Item, b: Item) => +a.statistics.viewCount - +b.statistics.viewCount);
-          // case '': return value;
           default: return value.filter((a: Item) => a.snippet.tags.join(' ').includes(args!));
         }
       }
