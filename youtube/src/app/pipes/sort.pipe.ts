862fe48e596f08d7794425c6ee8db7be8702cef0
Kate4ka007 <shlemena@mail.ru>
2022-10-09 13:42:30 +0300

    fix: change sorting

--- youtube/src/app/pipes/sort.pipe.ts
+++ youtube/src/app/pipes/sort.pipe.ts
@@ -8,16 +8,17 @@ export default class SortPipe implements PipeTransform {
   // eslint-disable-next-line class-methods-use-this, consistent-return
   transform(value: Item[] | undefined, args?: string): any {
     if (value) {
+      const values = [...value];
       if (args) {
         switch (args) {
-          case 'date': return value.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
-          case 'date-down': return value.sort((a: Item, b: Item) => new Date(b.snippet.publishedAt).getTime() - new Date(a.snippet.publishedAt).getTime());
-          case 'count': return value.sort((a: Item, b: Item) => +a.statistics.viewCount - +b.statistics.viewCount);
-          case 'count-down': return value.sort((a: Item, b: Item) => +b.statistics.viewCount - +a.statistics.viewCount);
-          default: return value.filter((a: Item) => a.snippet.tags.join(' ').includes(args!));
+          case 'date': return values.sort((a: Item, b: Item) => new Date(a.snippet.publishedAt).getTime() - new Date(b.snippet.publishedAt).getTime());
+          case 'date-down': return values.sort((a: Item, b: Item) => new Date(b.snippet.publishedAt).getTime() - new Date(a.snippet.publishedAt).getTime());
+          case 'count': return values.sort((a: Item, b: Item) => +a.statistics.viewCount - +b.statistics.viewCount);
+          case 'count-down': return values.sort((a: Item, b: Item) => +b.statistics.viewCount - +a.statistics.viewCount);
+          default: return values.filter((a: Item) => (`${a.snippet.tags.join(' ')} ${a.snippet.description} ${a.snippet.channelTitle}`).toLowerCase().includes(args!.toLowerCase()));
         }
       } else {
-        return value;
+        return values;
       }
     }
   }
