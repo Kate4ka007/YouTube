2286c3920ee1912082e616929e04a7bee8279214
Kate4ka007 <shlemena@mail.ru>
2022-10-19 09:01:47 +0300

    feat: add route reuse strategy

--- youtube/src/app/app.module.ts
+++ youtube/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouteReuseStrategy } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
+import { CustomRouteReuseStrategy } from './strategies/custom-route-reuse-strategy';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,10 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule,
   ],
   providers: [
-
+    {
+      provide: RouteReuseStrategy,
+      useClass: CustomRouteReuseStrategy,
+    },
   ],
   bootstrap: [AppComponent],
 })
--- youtube/src/app/core/models/data.ts
+++ youtube/src/app/core/models/data.ts
@@ -1,6 +1,8 @@
 // export const KEY_YOUTUBE = 'AIzaSyCOEDEHrTEywVl43l7FGgkd46-G0EgVq7s';
 
-export const KEY_YOUTUBE = 'AIzaSyAW8EIjpPJ-x3xzhxjgyM0ie0ksfXVD5O4';
+// export const KEY_YOUTUBE = 'AIzaSyAW8EIjpPJ-x3xzhxjgyM0ie0ksfXVD5O4';
+
+export const KEY_YOUTUBE = 'AIzaSyCHUaZZfGdfMvMDl7FKpnzXzX2PQ0RacQ0'
 export const BASE = 'https://www.googleapis.com/youtube/v3/';
 
 export const MAX_RESULTS = '12';
